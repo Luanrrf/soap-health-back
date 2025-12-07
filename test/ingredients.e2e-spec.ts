971aefb32e5d2cda38b334147c806770fb3f7291
Luan Ribeiro <Luanrrf@yahoo.com.br>
2025-12-07 12:14:40 -0300

    ci: improving ci tests

---
+++ test/ingredients.e2e-spec.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Ingredients (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ingredients (GET)', async () => {
+    const res = await request(app.getHttpServer()).get('/ingredients');
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0]).toHaveProperty('id');
+    expect(res.body[0]).toHaveProperty('name');
+  });
+});
